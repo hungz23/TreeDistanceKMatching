d41fdd33853bfc0cc4bc0d38623c5611a20f7898
hungz <hungz230103@gmail.com>
2018-05-10 16:46:04 +0800

    add MDkM in Tree

--- BFS.py
+++ BFS.py
@@ -4,7 +4,7 @@ Breadth First Search. See also LexBFS.py.
 
 D. Eppstein, May 2007.
 """
-
+import networkx as nx
 def BreadthFirstLevels(G,root):
     """
     Generate a sequence of bipartite directed graphs, each consisting
@@ -25,5 +25,47 @@ def BreadthFirstLevels(G,root):
                 if w not in visited:
                     levelGraph[v].add(w)
                     nextLevel.add(w)
-        yield levelGraph
+        print levelGraph
         currentLevel = nextLevel
+def BreadthFirstKLevels(G,root,k):
+    visited = set()
+    currentLevel = [root]
+    level = 0
+    nodes = []
+    while (currentLevel and level<=k):
+        for v in currentLevel:
+            visited.add(v)
+        nextLevel = set()
+        levelGraph = {v:set() for v in currentLevel}
+        for v in currentLevel:
+            for w in G[v]:
+                if w not in visited:
+                    levelGraph[v].add(w)
+                    nextLevel.add(w)
+        print levelGraph
+        nodes = nodes + levelGraph.keys()
+        level = level + 1
+        currentLevel = nextLevel
+    return nodes
+
+#Tree generator
+def _tree_edges(n,r):
+    # helper function for trees
+    # yields edges in rooted tree at 0 with n nodes and branching ratio r
+    nodes=iter(range(n))
+
+    parents=[next(nodes)] # stack of max length r
+    while parents:
+        source=parents.pop(0)
+        for i in range(r):
+            try:
+                target=next(nodes)
+                parents.append(target)
+                yield source,target
+            except StopIteration:
+                break
+#generate the tree graph
+n = 100
+G = nx.empty_graph(n)
+G.add_edges_from(_tree_edges(n,4))
+nodes = BreadthFirstKLevels(G,0,2)
--- InducedMatchingTree.py
+++ InducedMatchingTree.py
@@ -1,6 +1,6 @@
 import networkx as nx
 from LexBFS import LexBFS
-
+from BFS import BreadthFirstKLevels
 #Tree generator
 def _tree_edges(n,r):
     # helper function for trees
@@ -33,6 +33,8 @@ def induced_matching(G):
     visited = [False]*n
     M = []
     for u in sigma:
+        if visited[u]:
+            continue
         neighbors = sortlistwithmap(sigma, list(G.neighbors(u)))
         for node in neighbors:
             if(node_index(sigma, node)<node_index(sigma, u) and not visited[node]):
@@ -49,8 +51,33 @@ def induced_matching(G):
                 G.remove_nodes_from(Nv)
                 G.remove_nodes_from(Nu)
     return M
+def distancekmatching(G, k):
+    sigma = LexBFS(G)
+    visited = [False]*n
+    M = []
+    for u in sigma:
+        if visited[u]:
+            continue
+        neighbors = sortlistwithmap(sigma, list(G.neighbors(u)))
+        for node in neighbors:
+            if(node_index(sigma, node)<node_index(sigma, u) and not visited[node]):
+                v = node
+                M.append((v, u))
+                Nkv = BreadthFirstKLevels(G,v,k-1)
+                Nku = BreadthFirstKLevels(G,u,k-1)
+                for node in Nkv:
+                    visited[node] = True
+                for node in Nku:
+                    visited[node] = True
+                visited[u] = True
+                visited[v] = True
+                G.remove_nodes_from(Nkv)
+                G.remove_nodes_from(Nku)
+    return M
 #generate the tree graph
 n = 100
 G = nx.empty_graph(n)
 G.add_edges_from(_tree_edges(n,4))
 IM = induced_matching(G)
+k = 2
+DkM = distancekmatching(G, k)
